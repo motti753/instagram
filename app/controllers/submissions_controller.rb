588e6da09b70b14154188b74e63d2b6d8493645c
望月真紀 <yonetu.s.121@gmail.com>
2024-05-14 08:23:53 +0900

    投稿機能のnew, createの実装完了 indexのView表示でエラーあり。

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -11,6 +11,12 @@ class SubmissionsController < ApplicationController
 
   def create
     @submission = current_user.submissions.build(submission_params)
+    if @submission.save
+      redirect_to submissions_path, notice: '保存できました'
+    else
+      flash.now[:error] = '保存できません。コンテンツと画像を確かめてください'
+      render :new
+    end
   end
 
   private
