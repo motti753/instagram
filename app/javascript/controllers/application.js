ef61738762b262281e8406ea736cb11444af36cf
望月真紀 <yonetu.s.121@gmail.com>
2024-06-07 07:01:23 +0900

    follower, followingユーザ一覧機能実装完了

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -45,6 +45,63 @@ const handleHeartDisplay = (hasLiked) => {
 
 // main ------------------------------ //
 document.addEventListener('turbo:load', () => {
+  // const submission = document.getElementById('submission-show')
+  // const submissionId = submission.dataset.submissionId
+
+  // // like ------------------------------ //
+  // axios.get(`/api/submissions/${submissionId}/like`)
+  //   .then((response) => {
+  //     const hasLiked = response.data.hasLiked
+  //     handleHeartDisplay(hasLiked)
+  //   })
+  // // heartをinactive, activeと切り替える
+  // listenInactiveHeartEvent(submissionId)
+  // listenActiveHeartEvent(submissionId)
+  // // like ------------------------------ //
+
+  // // comment-btnが押されたら、comments_controller createを呼び出す
+  // $('.comment_btn').on('click', () => {
+  //   const comment3 = document.getElementById
+  //   ('comment-show3')
+  //   const commentUser = comment3.dataset.userId
+  //   const content = $('#comment_content').val()
+  //   if (!content){
+  //     window.alert('コメントを入力してください')
+  //   }else {
+  //     axios.post(`/api/submissions/${submissionId}/comments`, {
+  //       comment: {content: content, user_id: commentUser}
+  //     })
+  //       .then((response) => {
+  //         const comment = response.data
+  //         console.log(comment)
+  //         $('#comment_content').val('')
+  //       })
+  //   }
+  // })
+
+  // // comment --------------------------- //
+  // // comment一覧を表示
+  // const comment1 = document.getElementById('comment-show1')
+  // const comment2 = document.getElementById('comment-show2')
+
+  // const commentContent = JSON.parse(comment1.dataset.commentContent)
+  // const commentAvatar = JSON.parse(comment2.dataset.avatar)
+  // for(let i = 0; i < commentContent.length; i++){
+  //   appendNewComment(commentContent[i], commentAvatar[i])
+  // }
+  // // comment --------------------------- //
+
+  // // profile -------------------------- //
+  // const profile1 = document.getElementById('profile-show1')
+  // const profile2 = document.getElementById('profile-show2')
+
+  // const profileAccount = JSON.parse(profile1.dataset.profileAccount)
+  // const profileAvatar = JSON.parse(profile2.dataset.avatar)
+  // for(let i = 0; i < profileAccount.length; i++){
+  //   append_relationship(profileAccount[i], profileAvatar[i])
+  // }
+  // // profile -------------------------- //
+
   const currentUrl = window.location.href;
   if(currentUrl.includes('submissions')){
     const submission = document.getElementById('submission-show')
@@ -80,7 +137,7 @@ document.addEventListener('turbo:load', () => {
           })
       }
     })
-  }else if(currentUrl.includes('comment')){
+
     // comment --------------------------- //
     // comment一覧を表示
     const comment1 = document.getElementById('comment-show1')
